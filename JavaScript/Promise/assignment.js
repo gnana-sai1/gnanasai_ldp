5c37097060c839479497f7f0d30a54c6c4114cc2
Gnana Sai <gnanasai.talla@zemosolabs.com>
2023-06-06 13:36:20 +0530

    done promise assignment

---
+++ JavaScript/Promise/assignment.js
@@ -0,0 +1,22 @@
+function getData(uId) { 
+    return new Promise ((resolve,reject) => {
+        setTimeout(() => {
+            console.log("Fetched the data!");
+            resolve("skc@gmail.com");
+        }, 4000);
+    });
+}
+    
+console.log("start");
+getData("skc")
+    .then((email) => {
+        console.log("Email id of the user id is: " + email);
+        console.log("end");
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+
+
+
---
+++ JavaScript/Promise/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+console.log(promise.resolve);
+    
+// console.log("start");
+// getData("skc")
+//     .then((email) => {
+//         console.log("Email id of the user id is: " + email);
+//         console.log("end");
+//     })
+//     .catch((error) => {
+//         console.error(error);
+//     });
