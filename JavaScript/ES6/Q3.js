e4f6e73780d7a505b01b15827c765d38e97a16c1
Gnana Sai <gnanasai.talla@zemosolabs.com>
2023-06-23 15:25:48 +0530

    Made available

--- JavaScript/ES6/Q3.js
+++ JavaScript/ES6/Q3.js
@@ -2,12 +2,6 @@ const person = {
     name: 'Noam Chomsky',
     age: 92
 }
-
-// let name = person.name;
-// let age = person.age;
-// console.log(name);
-// console.log(age);
-
 const {name,age}= person;
 console.log(name);
 console.log(age);
